9c1d3e07c867ce8b13cc6b83e76ae7a307ee6c3f
Mojca Bozic <mojcacheer@gmail.com>
2025-04-24 00:15:58 +0200

    ustvarila tabele Pacient, Zdravnik, Oddelek, Pregled

---
+++ tables.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Pacient (
+    id_pacienta serial NOT NULL primary key,
+    uporabnisko_ime text NOT NULL,
+    geslo varchar NOT NULL,
+    starost int NOT NULL,
+    CHECK (starost >= 0),
+    spol varchar NOT NULL,
+    reden boolean NOT NULL
+);
+
+CREATE TABLE Oddelek (
+    id_oddelka serial NOT NULL primary key,
+    ime_oddelka varchar NOT NULL,
+    lokacija varchar NOT NULL
+);
+
+CREATE TABLE Zdravnik (
+    id_zdravnika serial NOT NULL primary key,
+    ime_zdravnika varchar NOT NULL,
+    oddelek int REFERENCES Oddelek(id_oddelka) 
+);
+
+CREATE TABLE Pregled (
+    id_pregleda serial NOT NULL primary key,
+    datum time NOT NULL,
+    cas text NOT NULL,
+    opis text,
+    pacient int REFERENCES Pacient(id_pacienta),
+    zdravnik int REFERENCES Zdravnik(id_zdravnika)
+);
