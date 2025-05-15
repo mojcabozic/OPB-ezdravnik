62935179c1cb47d590f81a6a4f11a4e1edb5a536
Mojca Bozic <mojcacheer@gmail.com>
2025-05-15 13:00:01 +0200

    dodala popravljeno verzijo models.py

--- Data/models.py
+++ Data/models.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass, field
 from dataclasses_json import dataclass_json
 from datetime import datetime
 
-<<<<<<< HEAD
 # definiramo razrede / podatkovne modele
 
 @dataclass_json
@@ -78,77 +77,3 @@ class pregledDto:
     zdravnik : int = field(default=0)
     
 
-=======
-# V tej datoteki definiramo vse podatkovne modele, ki jih bomo uporabljali v aplikaciji
-# Pazi na vrstni red anotacij razredov!
-
-@dataclass_json
-@dataclass
-class Oddelek:
-    id_oddelka: int = field(default=0)
-    ime_oddelka: str = field(default="")
-    lokacija: str = field(default="")
-
-@dataclass_json
-@dataclass
-class OddelekDto:
-    id_oddelka: int = field(default=0)
-    ime_oddelka: str = field(default="")
-    lokacija: str = field(default="")
-
-@dataclass_json
-@dataclass
-class Zdravnik:
-    id_zdravnika: int = field(default=0)
-    ime_zdravnika: str = field(default="")
-    oddelek_id: int = field(default=0)
-
-@dataclass_json
-@dataclass
-class ZdravnikDto:
-    id_zdravnika: int = field(default=0)
-    ime_zdravnika: str = field(default="")
-    oddelek_id: int = field(default=0)   
-
-@dataclass_json
-@dataclass
-class Pacient:
-    ime_pacienta: str = field(default="")
-    id_pacienta: int = field(default=0)
-    email: str = field(default="")
-    starost: int = field(default=0)
-    spol: str = field(default="")
-    redno: bool = field(default=False)
-
-@dataclass_json
-@dataclass
-class PacientDto:
-    ime_pacienta: str = field(default="")
-    id_pacienta: int = field(default=0)
-    emso : int = field(default = "")
-    email: str = field(default="")
-    geslo: str = field(default="")
-    starost: int = field(default=0)
-    spol: str = field(default="")
-    redno: bool = field(default=False)
-
-@dataclass_json
-@dataclass
-class Pregled:
-    id_pregleda: int = field(default=0)
-    datum_pregleda: datetime = field(default_factory=datetime.now)
-    termin_pregleda: str = field(default="")  
-    pacient_id: int = field(default=0)       
-    zdravnik_id: int = field(default=0)       
-
-
-@dataclass_json
-@dataclass
-class PregledDto:
-    id_pregleda: int = field(default=0)
-    ime_pacienta: str = field(default="")
-    ime_zdravnika: str = field(default="")
-    datum_pregleda: datetime = field(default_factory=datetime.now)
-    opis_tezave: str = field(default="")
-    termin_pregleda: str = field(default="")
->>>>>>> c67a7a0dd4bcccac875e22dc23175318251b8b0e
