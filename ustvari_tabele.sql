08b10a567d5bce3b695da82271c692cfadc9d8a5
Mojca Bozic <mojcacheer@gmail.com>
2025-05-29 20:22:18 +0200

    avtomatski zapis v bazo iz csv datoteke

---
+++ ustvari_tabele.sql
@@ -0,0 +1,42 @@
+DROP TABLE IF EXISTS Pregled;
+DROP TABLE IF EXISTS Zdravnik;
+DROP TABLE IF EXISTS Oddelek;
+DROP TABLE IF EXISTS Lokacija;
+DROP TABLE IF EXISTS Pacient;
+
+CREATE TABLE Pacient (
+    id_pacienta integer NOT NULL primary key,
+    ime_pacienta varchar NOT NULL,
+    uporabnisko_ime text NOT NULL,
+    geslo varchar NOT NULL, 
+    geslo_hash varchar NOT NULL,
+    datum_rojstva date NOT NULL,
+    spol varchar NOT NULL,
+    reden boolean NOT NULL
+);
+
+CREATE TABLE Lokacija (
+    id_lokacije integer NOT NULL primary key,
+    naslov text NOT NULL
+);
+
+CREATE TABLE Oddelek (
+    id_oddelka integer NOT NULL primary key,
+    ime_oddelka varchar NOT NULL,
+    lokacija int REFERENCES Lokacija(id_lokacije)
+);
+
+CREATE TABLE Zdravnik (
+    id_zdravnika integer NOT NULL primary key,
+    ime_zdravnika varchar NOT NULL,
+    oddelek int REFERENCES Oddelek(id_oddelka) 
+);
+
+CREATE TABLE Pregled (
+    id_pregleda integer NOT NULL primary key,
+    datum time NOT NULL,
+    cas text NOT NULL,
+    opis text,
+    pacient int REFERENCES Pacient(id_pacienta),
+    zdravnik int REFERENCES Zdravnik(id_zdravnika)
+);
---
+++ zapisi_iz_csv.py
@@ -0,0 +1,78 @@
+import psycopg2
+from psycopg2 import sql
+import pandas  as pd
+from re import sub
+# uvozimo psycopg2
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+
+import Data.auth_public as auth
+import csv
+
+# Ustvarimo povezavo
+conn = psycopg2.connect(
+    database = auth.db,
+    host = auth.host,
+    port = auth.port,
+    user = auth.user,
+    password = auth.password,
+)
+
+
+# Iz povezave naredimo cursor, ki omogoča
+# zaganjanje ukazov na bazi
+
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+
+def get_datatypes(table_name):
+    cur.execute("""                              
+        SELECT column_name, data_type, is_nullable
+        FROM information_schema.columns
+        WHERE table_name = %s;
+    """, (table_name,))
+
+    schema = cur.fetchall()
+
+    # naredimo slovar, ki ima za ključe imena stolpcev, za vrednosti pa njihov data type
+    out = {}
+    for item in schema:
+        key = item[0]
+        data_type = item[1]
+        out[key] = data_type
+    return out
+    
+tabele = [
+    "lokacija",
+    "oddelek",
+    "zdravnik"
+]
+
+for ime_tabele in tabele:
+    podatkovni_tipi = get_datatypes(ime_tabele) # slovar 
+    print(ime_tabele, podatkovni_tipi)
+
+    # Open the CSV file and use DictReader
+    with open(f"csv/{ime_tabele}.csv", 'r', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            columns = row.keys()
+
+            # Pretvorimo podatkovni tip 
+            for column in columns:
+                if podatkovni_tipi[column] == 'integer':
+                    row[column] = int(row[column])
+
+            query = sql.SQL("INSERT INTO {table} ({fields}) VALUES ({placeholders})").format(
+                table=sql.Identifier(ime_tabele),
+                fields=sql.SQL(', ').join(map(sql.Identifier, columns)),
+                # placeholder je vec al manj %s
+                placeholders=sql.SQL(', ').join(sql.Placeholder() * len(columns))
+            )
+            cur.execute(query, list(row.values()))
+
+
+conn.commit()
+cur.close()
+conn.close()
+print("CSV data inserted successfully.")
